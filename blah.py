8e5f7dc7f474389ec16ff1a69a7e107b3c32a3da
Soups71 <kidsoups@gmail.com>
2021-01-20 21:12:13 -0500

    Code that might be helpful

---
+++ blah.py
@@ -0,0 +1,17 @@
+
+import psutil
+
+def get_interfaces():
+    addresses = psutil.net_if_addrs()
+    stats = psutil.net_if_stats()
+
+    wireless_interfaces = []
+    for intface, addr_list in addresses.items():
+        if  intface[:2] == "wl":
+            wireless_interfaces.append(intface)
+    return wireless_interfaces
+
+
+if __name__ == "__main__":
+    interfaces = get_interfaces()
+    print(interfaces[1:])
